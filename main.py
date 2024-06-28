e90cdfbcb2f6f9eea3e5253c841483e8245d67bb
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-27 20:46:15 -0700

    Lab 4-4

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    number=0
+    while True:
+        try:
+            number= int(input('Enter number:'))
+            if 1 <= number <= 100: 
+                break 
+            else: 
+                print('retry with appropriate number')
+        except ValueError:
+            print('Must enter number')
     print(number)
+
+        
+        
+                
+
+        
 
     ########################################
     # Do not delete the return statement
